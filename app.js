c8c0545281624d8511b6977413a5294a774a1b94
Prasanna Prakhar <prasannaprakhar@Prasannas-MacBook-Air.local>
2025-02-25 18:51:00 +0530

    cors added

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const notificationController = require('./controllers/notificatonController');
 
 const app = express();
 // const port = process.env.PORT || 6000;
+
+// Use CORS middleware
+app.use(cors());
 
 // Middleware
 app.use(bodyParser.json());
