eada5e5ca1ce6f861657ab3cfafa95013c12fbc1
Prasanna Prakhar <prasannaprakhar@Prasannas-MacBook-Air.local>
2025-02-25 11:02:58 +0530

    port change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('./app');
 // const { runConsumer } = require('./services/kafkaConsumer');
 
 // Start the express server
-const port = 6000;
+const port = 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
   // runConsumer().catch(console.error);
