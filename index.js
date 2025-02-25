25c34f325d3517a7ca6f0bd3abba4de995b5c2e6
Prasanna Prakhar <prasannaprakhar@Prasannas-MacBook-Air.local>
2025-02-25 14:45:17 +0530

    health check

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = require('./app');
 
 // Start the express server
 const port = 6000;
-app.listen(port, () => {
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+//   // runConsumer().catch(console.error);
+// });
+
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
-  // runConsumer().catch(console.error);
 });
